fc502e4ecc00cf5cdd39036f5771bfaf969370ea
Osama M'hiri <59898800+Ryder05@users.noreply.github.com>
2022-12-08 21:58:19 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,6 +1,14 @@
 #!/bin/bash 
 
 echo "this is the the secret grabbed from azure key vault"
-x=$1
+if [[ -z $1 ]];
+then 
+    echo "No parameter passed."
+else
+    echo "Parameter passed = $1"
+    x=$1
 echo ${x}
 echo ${#x}
+
+fi
+
